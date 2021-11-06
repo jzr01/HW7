8d9453656ec78c2888050a5f21d56653570e0a91
jzr01 <70544438+jzr01@users.noreply.github.com>
2021-11-06 12:11:15 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import altair as alt
-from sklearn.datasets import make_blobs
+from sklearn
 from sklearn.cluster import KMeans
 import streamlit as st
 
@@ -18,7 +18,7 @@ if __name__ == '__main__':
 
     iterations = st.slider('Choose the number of iteration',1,100,1)
 
-    X, _ = make_blobs(n_samples=1000, centers=5, n_features=2, random_state = 1)
+    X, _ = sklearn.datasets.make_blobs(n_samples=1000, centers=5, n_features=2, random_state = 1)
     df = pd.DataFrame(X, columns = list("ab"))
     starting_points = np.array([[0,0],[-2,0],[-4,0],[0,2],[0,4]])
     kmeans = KMeans(n_clusters = 5, max_iter= iterations, init=starting_points, n_init = 1)
